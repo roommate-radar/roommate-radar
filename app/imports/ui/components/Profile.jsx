d5ecc1470ab2d144673a17c544a23fba74cbed82
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2021-11-16 10:22:25 -1000

    Issue-02 Completed

---
+++ app/imports/ui/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Profile extends React.Component {
+  render() {
+    const prop = this.props.profile;
+    return (
+      <Card href={`/profile/${prop._id}`}>
+        <Image src={prop.image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{prop.lastName}, {prop.firstName}</Card.Header>
+          <Card.Meta>
+            Major: {prop.major}, Class of {prop.year}
+          </Card.Meta>
+          <Card.Description>
+            {prop.description}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(Profile);
