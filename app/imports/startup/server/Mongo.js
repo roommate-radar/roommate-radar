0653199d208611dbdda32213617f2700209e36cd
Somewha7 <35878642+Somewha7@users.noreply.github.com>
2021-11-18 10:07:16 -1000

    removed extraneous references

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Profiles } from '../../api/profiles/Profiles';
 
 /* eslint-disable no-console */
@@ -8,20 +7,6 @@ import { Profiles } from '../../api/profiles/Profiles';
 function addProfile(profile) {
   console.log(`  Adding: ${profile.firstName} ${profile.lastName} (${profile.owner})`);
   Profiles.collection.insert(profile);
-}
-
-// Initialize the database with a default profile document.
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-}
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
-  }
 }
 
 // Initialize the ProfilesCollection if empty.
