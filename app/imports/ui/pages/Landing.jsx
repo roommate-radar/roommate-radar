c482b1373a03d8626d33dd7509e7805cb104afe5
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2021-11-17 20:50:05 -1000

    Landing Background
    Added mock background image

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,33 +6,35 @@ import { Link } from 'react-router-dom';
 class Landing extends React.Component {
   render() {
     return (
-      <Grid container centered columns={2}>
-        <Grid.Column textAlign='center'>
-          <Header as='h1'>Welcome to Room-mateRadar</Header>
-          <Header as='h3'> A web application that helps Students at the University of Hawaii Manoa find a roommate. </Header>
-        </Grid.Column>
+      <div className='room-mateRadar'>
+        <Grid container centered columns={2}>
+          <Grid.Column textAlign='center'>
+            <Header as='h1'>Welcome to Room-mateRadar</Header>
+            <Header as='h3'> A web application that helps Students at the University of Hawaii Manoa find a roommate. </Header>
+          </Grid.Column>
 
-        <Grid container centered stackable columns='2'>
-          <Grid.Row>
-            <Grid.Column textAlign='center'>
-              <Icon size='huge' name='users'/>
-              <Header as='h1'>Multiple Users</Header>
-              <Header as='h3'>Create your own personal profile to display to other students.</Header>
-            </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Icon size='huge' name='calendar check'/>
-              <Header as='h1'>Personalize</Header>
-              <Header as='h3'>List specific personal details like pet preference, budget, neighborhood details to narrow down suitable room-mate.</Header>
+          <Grid container centered stackable columns='2'>
+            <Grid.Row>
+              <Grid.Column textAlign='center'>
+                <Icon size='huge' name='users'/>
+                <Header as='h1'>Multiple Users</Header>
+                <Header as='h3'>Create your own personal profile to display to other students.</Header>
+              </Grid.Column>
+              <Grid.Column textAlign='center'>
+                <Icon size='huge' name='calendar check'/>
+                <Header as='h1'>Personalize</Header>
+                <Header as='h3'>List specific personal details like pet preference, budget, neighborhood details to narrow down suitable room-mate.</Header>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
+          <Grid.Row columns={1} centered>
+            <Grid.Column verticalAlign='middle'>
+              <Header as='h4'>To continue, please login in <Link to='/'>here</Link>. Don&apos;t have an account with us? Please register <Link to='/'>here</Link></Header>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-
-        <Grid.Row columns={1} centered>
-          <Grid.Column verticalAlign='middle'>
-            <Header as='h4'>To continue, please login in <Link to='/'>here</Link>. Don&apos;t have an account with us? Please register <Link to='/'>here</Link></Header>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      </div>
     );
   }
 }
