a55a873da051e949480594e07324bb596b2f81ac
Somewha7 <35878642+Somewha7@users.noreply.github.com>
2021-11-17 20:43:40 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,8 +8,9 @@ class Landing extends React.Component {
     return (
       <Grid container centered columns={2}>
         <Grid.Column textAlign='center'>
-          <Header as='h1'>Welcome to Room-mateRadar</Header>
-          <Header as='h3'> A web application that helps Students at the University of Hawaii Manoa find a roommate. </Header>
+          <Header as='h1'>Welcome to Roommate Radar</Header>
+          <Header as='h3'> Roommate Radar has helped students find the perfect roommate since 2021.
+            Designed by students for students, Roommate Radar is the perfect platform to meet others like you.</Header>
         </Grid.Column>
 
         <Grid container centered stackable columns='2'>
@@ -22,14 +23,14 @@ class Landing extends React.Component {
             <Grid.Column textAlign='center'>
               <Icon size='huge' name='calendar check'/>
               <Header as='h1'>Personalize</Header>
-              <Header as='h3'>List specific personal details like pet preference, budget, neighborhood details to narrow down suitable room-mate.</Header>
+              <Header as='h3'>List specific personal details like pet preference, budget, neighborhood details to help find the perfect roommate.</Header>
             </Grid.Column>
           </Grid.Row>
         </Grid>
 
         <Grid.Row columns={1} centered>
           <Grid.Column verticalAlign='middle'>
-            <Header as='h4'>To continue, please login in <Link to='/'>here</Link>. Don&apos;t have an account with us? Please register <Link to='/'>here</Link></Header>
+            <Header as='h4'>To continue, please <Link to='/'>log in</Link>. Don&apos;t have an account with us? Register <Link to='/'>here</Link></Header>
           </Grid.Column>
         </Grid.Row>
       </Grid>
