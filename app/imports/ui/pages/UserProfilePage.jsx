8bf5149079647b32826b4bfa7668fd7e95f95097
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2021-11-21 05:26:52 -1000

    Fixed MyProfile
    * Why clicked on cards it should redirect to other profiles.
    * Clicking Myprofile redirect to User profile (only newly created users)
    *does not work when log in to defaultAccounts

--- app/imports/ui/pages/UserProfilePage.jsx
+++ app/imports/ui/pages/UserProfilePage.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { _ } from 'meteor/underscore';
 import { Container, Loader, Card } from 'semantic-ui-react';
 import { Profiles } from '../../api/profiles/Profiles';
 import Profile from '../components/Profile';
+
+function getProfileId(param1, param2) {
+  let data = '';
+  if (param1 === param2) {
+    data = Profiles.collection.find({ owner: param2 }).fetch();
+  } else {
+    data = Profiles.collection.find({ _id: param2 }).fetch();
+  }
+  return data;
+}
 
 /** Renders the selected user profile page. */
 class UserProfilePage extends React.Component {
@@ -13,10 +24,13 @@ class UserProfilePage extends React.Component {
   }
 
   renderPage() {
+    const currentId = Meteor.user().username;
+    const parameter = this.props.paramId;
+    const currentUser = getProfileId(currentId, parameter);
     return (
       <Container>
         <Card.Group>
-          {this.props.profiles.map((profile, index) => <Profile key={index} profile={profile}/>)}
+          {_.map(currentUser, (profile, index) => <Profile key={index} profile={profile}/>)}
         </Card.Group>
       </Container>
     );
@@ -25,23 +39,19 @@ class UserProfilePage extends React.Component {
 
 // Require a object of student profile to be passed to this component.
 UserProfilePage.propTypes = {
-  profiles: PropTypes.array.isRequired,
+  paramId: PropTypes.string,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. */
 export default withTracker(({ match }) => {
-  const currentProfile = match.params._id;
+  const paramId = match.params._id;
   // Ensure access to User's profile page
   const subscription = Meteor.subscribe(Profiles.userPublicationName);
-  // Tester
-  // console.log(currentProfile);
-  // Get the Profiles collection
-  const profiles = Profiles.collection.find({ _id: currentProfile }).fetch();
   // Determine if the subscription is ready
   const ready = subscription.ready();
   return {
-    profiles,
+    paramId,
     ready,
   };
 })(UserProfilePage);
