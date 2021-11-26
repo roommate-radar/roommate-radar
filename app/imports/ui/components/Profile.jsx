37542265bc71d66d687a70abc210b567aa46c9ec
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2021-11-25 21:10:34 -1000

    Update Profile.jsx
    * realize don't need the function at all to get the owner.

--- app/imports/ui/components/Profile.jsx
+++ app/imports/ui/components/Profile.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { _ } from 'meteor/underscore';
-import { Profiles } from '../../api/profiles/Profiles';
 
-function getUsersProfile(param1) {
-  const data = Profiles.collection.find({}).fetch();
-  const getUserName = _.pluck(data.filter(datas => datas._id === param1), 'owner');
-  return getUserName;
-}
+// function getUsersProfile(param1) {
+//   const data = Profiles.collection.find({}).fetch();
+//   const getUserName = _.pluck(data.filter(datas => datas._id === param1), 'owner');
+//   return getUserName;
+// }
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Profile extends React.Component {
   render() {
     const prop = this.props.profile;
-    const user_name = getUsersProfile(prop._id);
     return (
-      <Card href={`#/profile/${user_name}`}>
+      <Card href={`#/profile/${prop.owner}`}>
         <Image src={prop.image} wrapped ui={false} />
         <Card.Content>
           <Card.Header>{prop.lastName}, {prop.firstName}</Card.Header>
