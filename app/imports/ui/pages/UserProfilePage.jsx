a4558f1d2a3c44ae2a262ac24aabaf51623b7bde
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2021-11-29 21:32:36 -1000

    Updated UserProfilePage
    * Made a UserProfile Component to display UserProfilePage, code looked messy.
    *Linked edit into logged user

--- app/imports/ui/pages/UserProfilePage.jsx
+++ app/imports/ui/pages/UserProfilePage.jsx
@@ -3,18 +3,15 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
-import { Container, Loader, Grid, Header, Image } from 'semantic-ui-react';
+import { Container, Loader, Grid } from 'semantic-ui-react';
 import { Profiles } from '../../api/profiles/Profiles';
+import UserProfiles from '../components/UserProfiles';
 
-// function getProfileId(param1, param2) {
-//   let data = '';
-//   if (param1 === param2) {
-//     data = Profiles.collection.find({ owner: param2 }).fetch();
-//   } else {
-//     data = Profiles.collection.find({ owner: param2 }).fetch();
-//   }
-//   return data;
-// }
+function getProfileId(param1) {
+  const data = Profiles.collection.find({}).fetch().filter(datas => datas.owner === param1[0]);
+  const emails = _.pluck(data, 'owner');
+  return emails[0];
+}
 
 /** Renders the selected user profile page. */
 class UserProfilePage extends React.Component {
@@ -23,42 +20,13 @@ class UserProfilePage extends React.Component {
   }
 
   renderPage() {
-    // const currentId = Meteor.user().username;
-    // const parameter = this.props.paramId;
-    // {_.map(currentUser1, (profile, index) => <Profile key={index} profile={profile}/>)}
-    const currentUser1 = Profiles.collection.find({ owner: this.props.paramId }).fetch();
-    // console.log(currentUser1);
+    const userEmail = getProfileId(_.pluck(Meteor.user().emails, 'address'));
+    const getData = (this.props.paramId === Meteor.user().username) ? userEmail : this.props.paramId;
+    const currentUser1 = Profiles.collection.find({ owner: getData }).fetch();
     return (
       <Container>
-        <Grid celled column={2}>
-          <Grid.Row>
-            <Grid.Column width={6}>
-              <Image circular src={_.pluck(currentUser1, 'image')} size='medium'/>
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Header inverted as='h1'>
-                <div>{_.pluck(currentUser1, 'firstName')} {_.pluck(currentUser1, 'lastName')}</div>
-              </Header>
-              <Header inverted as='h3'>
-                About Me <br/><br/>
-                <div>{_.pluck(currentUser1, 'description')}</div>
-              </Header>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row column={1}>
-            <Grid.Column>
-              <Header inverted as='h2'>
-                <div>Major: {_.pluck(currentUser1, 'major')} </div>
-              </Header>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row column={1}>
-            <Grid.Column>
-              <Header inverted as='h2'>
-                <div>Class of: {_.pluck(currentUser1, 'year')} </div>
-              </Header>
-            </Grid.Column>
-          </Grid.Row>
+        <Grid>
+          {_.map(currentUser1, (profile, index) => <UserProfiles key={index} profile={profile}/>)}
         </Grid>
       </Container>
     );
@@ -66,11 +34,10 @@ class UserProfilePage extends React.Component {
 }
 
 // Require a object of student profile to be passed to this component.
-UserProfilePage.propTypes =
-  {
-    paramId: PropTypes.string,
-    ready: PropTypes.bool.isRequired,
-  };
+UserProfilePage.propTypes = {
+  paramId: PropTypes.string,
+  ready: PropTypes.bool.isRequired,
+};
 
 /** withTracker connects Meteor data to React components. */
 export default withTracker(({ match }) => {
