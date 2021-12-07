35970fcf1e43588bec7e3f59e2dd67d530e21d6b
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2021-12-07 02:11:26 -1000

    Worked on Issue #18
    * Description should be showing parts  in list and full on my profile
    *tweak edit profile so it supports data with , and without
    *still need to implement a way to catch user inputs. like separating types of animals with , and not having similar pets in black and white list

--- app/imports/ui/components/Profile.jsx
+++ app/imports/ui/components/Profile.jsx
@@ -3,26 +3,28 @@ import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
-// function getUsersProfile(param1) {
-//   const data = Profiles.collection.find({}).fetch();
-//   const getUserName = _.pluck(data.filter(datas => datas._id === param1), 'owner');
-//   return getUserName;
-// }
+function truncateString(str) {
+  if (str.length > 65) {
+    return `${str.slice(0, 65)}...`;
+  }
+  return str;
+}
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Profile extends React.Component {
   render() {
     const prop = this.props.profile;
+    const description = truncateString(prop.description);
     return (
       <Card href={`#/profile/${prop.owner}`}>
-        <Image src={prop.image} wrapped ui={false} />
+        <Image size='medium' src={prop.image} wrapped ui={false}/>
         <Card.Content>
           <Card.Header>{prop.lastName}, {prop.firstName}</Card.Header>
           <Card.Meta>
             Major: {prop.major}, Class of {prop.year}
           </Card.Meta>
           <Card.Description>
-            {prop.description}
+            {description}
           </Card.Description>
         </Card.Content>
       </Card>
